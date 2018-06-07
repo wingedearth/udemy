bf024625a83c3b8fce2f1893db235171fbf41260
Andrew Anissi <andrew.anissi@nbcuni.com>
2018-06-06 23:21:45 -0700

    add chat component and styling

--- cards/src/client/components/lobby.js
+++ cards/src/client/components/lobby.js
@@ -1,10 +1,45 @@
 import './lobby.scss';
 import React, {Component} from 'react';
+import Chat from './chat';
 
 class LobbyContainer extends Component {
+	constructor(props) {
+		super(props);
+
+		this._joinGame = game => {
+			console.log(`TODO: JOIN GAME ${game.title}`);
+		};
+
+		this._sendMessage = message => {
+			console.log(`Sending ${message}`);
+		};
+	}
+
 	render() {
+		const games = [
+			{title: "Game 1", id: 1, players: ["Andy", "Jenn", "Caspy"]},
+			{title: "Game 2", id: 2, players: ["Andrew", "Jennifer", "Caspian"]},
+			{title: "Game 3", id: 3, players: ["Arash", "Guinevere", "Arthur"]},
+			{title: "Game 4", id: 4, players: ["Daddy", "Mommy", "Baby"]}
+		];
+
+		const opSendMessage = {can: true, inProgress: false};
+		const messages = [
+			{index: 1, name: "Louis", message: "Woof"},
+			{index: 2, name: "Andrew", message: "Blah"},
+			{index: 3, name: "Jennifer", message: "Hmf"},
+			{index: 4, name: "Caspian", message: "Mah"},
+			{index: 5, name: "Casey", message: "Arf"}
+		];
+
 		return (
-			<p>Lobby!</p>
+			<div className="c-lobby">
+				<GameList games={games} joinGame={this._joinGame} />
+				<Chat messages={messages} 
+				opSendMessage={opSendMessage}
+				sendMessage={this._sendMessage}
+				/>
+			</div>
 		);
 	}
 }
@@ -29,7 +64,7 @@ class LobbySidebar extends Component {
 
 		return (
 			<section className="c-lobby-sidebar">
-				<div className="m-sidebar-actions">
+				<div className="m-sidebar-buttons">
 					{!canLogin ? null :
 						<button className="m-button primary" onClick={this._login}>
 							Login
@@ -45,6 +80,24 @@ class LobbySidebar extends Component {
 			</section>
 		);
 	}
+}
+
+function GameList({games, joinGame}) {
+	return (
+		<section className="c-game-list">
+			{games.length > 0 ? null :
+				<div className="no-games">There are no games yet :(</div>}
+
+			{games.map(game =>
+			<div className="game" key={game.id} onClick={() => joinGame(game)}>
+				<div className="title">{game.title}</div>
+				<div className="players">
+					{game.players.join(", ")}
+				</div>
+			<div className="join-game">Join Game</div>
+			</div>)}
+		</section>
+	);
 }
 
 export default {
